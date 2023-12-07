b929e944a188240995fa9d1772eaab25e818293d
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-07 12:21:07 +0500

    Hello Javascript. 1-lesson homework is done.

---
+++ script.js
@@ -0,0 +1,70 @@
+// 1-TOPSHIRIQ. 
+// 1-SAVOL Foydalanuvchidan 3ta son kiritishini so'rang va va shu sonlarning yig'indisini toping.
+
+// let firstNumber = +prompt("1-sonni kiriting.");
+// let secondNumber = +prompt("2-sonni kiriting.");
+// let ThirdNumber = +prompt("3-sonni kiriting.");
+
+// let result = firstNumber+secondNumber+ThirdNumber;
+
+// console.log(alert(result));
+
+/*************************************************** */
+
+// 2-TOPSHIRIQ. 
+// 1-SAVOL Foydalanuvchidan 2ta son kiritishini so'rang va va shu sonlarning kvadratlari  yig'indisini toping.
+
+// let firstNumber = +prompt("1-sonni kiriting.");
+// let secondNumber = +prompt("2-sonni kiriting.");
+
+// let result = firstNumber*firstNumber+secondNumber*secondNumber;
+
+// console.log(alert(result));
+
+/*************************************************** */
+
+// 3-TOPSHIRIQ. 
+// 1-SAVOL Ixtiyoriy to'g'ri to'rtburchakning perimetrini topuvchi algoritm yozing.
+
+// let firstNumber = +prompt("1-tomonni kiriting.");
+// let secondNumber = +prompt("2-tomonni kiriting.");
+// let ThirdNumber = +prompt("3-tomonni kiriting.");
+// let fourthNumber = +prompt("4-tomonni kiriting.");
+
+// let result = firstNumber+secondNumber+ThirdNumber+fourthNumber;
+
+// console.log(alert( result));
+
+/*************************************************** */
+
+
+// 4-TOPSHIRIQ. 
+// 1-SAVOL Kubning tomoni berilsa uning hajmi hamda to'la sirtini  topuvchi algoritm yozing.
+
+// let firstNumber = +prompt("Kubning 1-tomonini kiriting.");
+
+// let result = firstNumber*firstNumber*firstNumber;
+
+// console.log(alert( result));
+
+/*************************************************** */
+
+// 4-TOPSHIRIQ. 
+// 2-SAVOL Kubning tomoni berilsa uning hajmi hamda to'la sirtini topuvchi algoritm yozing.
+
+// let firstNumber = +prompt("Kubning 1-tomonini uzunligini kiriting.");
+
+// let result = 6*firstNumber*firstNumber;
+
+// console.log(alert( result));
+
+// 5-TOPSHIRIQ. 
+// 1-SAVOL  topuvchi algoritm yozing.
+
+// let firstNumber = +prompt("Doiraning diametirini kiriting.");
+
+// let result = 3.14*firstNumber/4;
+
+// console.log(alert( result));
+
+ 
